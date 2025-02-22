be39c89f8550a5707ef5e851ade669321dbf542c
SumanthMeenan <sumanthmeenan.kanneti123@gmail.com>
2025-02-22 07:03:07 -0500

    api key in .env file

--- configure.py
+++ configure.py
@@ -1,11 +1,19 @@
 import google.generativeai as genai
 import os 
 
+from dotenv import load_dotenv
+import os
+    
 def configure_api():
     
     print("Configuring Gemini Connection using Google API Key")
-    os.environ["GOOGLE_API_KEY"] = ""
-    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-    genai.configure(api_key=GOOGLE_API_KEY)
+    load_dotenv()  # Loads environment variables from .env file
+    google_api_key = os.getenv("GOOGLE_API_KEY")
+
+    if google_api_key:
+        print("API Key retrieved successfully!")
+    else:
+        print("API Key not found.")
+    genai.configure(api_key=google_api_key)
     print("Successfully Configured")
    
--- video_analyzer.py
+++ video_analyzer.py
@@ -1,46 +1,10 @@
 import os
-import PIL 
 import google.generativeai as genai
-from PIL import Image, ImageDraw
-import matplotlib.pyplot as plt
-import re
-import time
-from typing_extensions import TypedDict
-from typing import Literal
-from model import model 
+from model import model, model2
 
 # Choose a Gemini model.
 from configure import configure_api
 configure_api() 
-#model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
-
-# # Create a prompt to detect bounding boxes.
-# prompt = "Return a bounding box for each of the objects in this image in [ymin, xmin, ymax, xmax] format."
-# sample_file_2 = PIL.Image.open('data/piranha.jpg')
-# response = model.generate_content([sample_file_2, prompt])
-# print(response)
-
-import requests
-
-# Function to download video from a URL
-def download_video(video_url, filename):
-    response = requests.get(video_url, stream=True)
-    if response.status_code == 200:
-        with open(filename, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=1024):
-                f.write(chunk)
-        print(f"Video downloaded: {filename}")
-    else:
-        print("Failed to download video")
-
-# Example video URL
-video_url = 'https://www.youtube.com/watch?v=C5ZDl4epfbM'
-local_video_filename = 'data/tennis_1.mp4'
-
-# Download the video
-download_video(video_url, local_video_filename)
-print("video downloaded")
-
 
 video_extensions = ['.mp4', '.avi', '.mov', '.mkv', '.flv', '.wmv']
 # Select only videos from the list
@@ -48,31 +12,11 @@ files = os.listdir("data/")
 video_files = [file for file in files if any(file.lower().endswith(ext) for ext in video_extensions)]
 
 # generate and print response
-# response = model.generate_content(["Analyse this video", video_files[0]],
-#                                   request_options={"timeout": 600})
-
-print(video_files)
-response = model.generate_content(["Analyse this video", "tennis_1.mp4"],
+print(video_files[0])
+response = model.generate_content(["Analyse this video", video_files[0]],
                                   request_options={"timeout": 600})
 
 print(response.text)
-
-
-import cv2
-import google.generativeai as genai
-from PIL import Image
-import numpy as np
-
-# Initialize Gemini model
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
-import cv2
-from PIL import Image
-from model import model2 
-
-# Initialize model (ensure you are correctly passing the model name or using the right model from the gemini API)
-
-# # Create the prompt.
-# prompt = "Summarize this video. Then create a quiz with answer key based on the information in the video."
 
 files = genai.list_files()
 for file in files:
@@ -84,12 +28,7 @@ print(f"Retrieved File ID: {retrieved_file.name}, Status: {retrieved_file.state.
 response2 = model2.generate_content(["Analyse this video", retrieved_file],
                                   request_options={"timeout": 600})
 print("response 2: ", response2.text)
-# # Make the LLM request.
-# print("Making LLM inference request...")
-# response = model.generate_content([video_file, prompt],
-#                                   request_options={"timeout": 600})
 
-# print(response)
 
 
 
